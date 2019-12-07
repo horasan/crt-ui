42ab23f181c2ca327ff8d1d21f09c1c5d4c4617d
Horasan, Riza <rizahorasan@gmail.com>
2019-12-07 02:57:17 +0000

    header, file_upload and search components are created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FileUploadComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
