29ac77a082123a4fe57435fd82ae45aa4ab3daf5
Horasan, Riza <rizahorasan@gmail.com>
2019-12-07 05:18:13 +0000

    Sample get

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    /*this.http
+            .get<Course[]>("/courses.json")
+            .map(data => _.values(data))
+            .do(console.log);*/
+
+            this.http.get('http://localhost:5000/competitionapi/v1/competition/ranking/1')
+            .subscribe(responseData => console.log(responseData));
   }
 
 }
