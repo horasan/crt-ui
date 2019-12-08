94af400b1f526477b501e2e7cb43d3387d62d77e
Horasan, Riza <rizahorasan@gmail.com>
2019-12-08 22:40:15 +0000

    File validations are addded.

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -9,80 +9,60 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FileUploadComponent implements OnInit {
 
-  public fileName: string= 'fileName1';
+  public fileName: string = 'fileName1';
   public numberOfLines: number = 10;
   public fileUploadResult: string = 'File not is not sent yet!';
-  allowFileUpload = false;
-
-
+  allowFileUpload: boolean = false;
+  previewUrl: boolean = true;
+  fileSizeError: boolean = false;
   fileData: File = null;
-  previewUrl:any = null;
   fileUploadProgress: string = null;
   uploadedFilePath: string = null;
   constructor(private http: HttpClient) { }
 
+
   fileProgress(fileInput: any) {
     this.fileData = <File>fileInput.target.files[0];
-    this.preview();
-}
 
-preview() {
-  // Show preview 
-  var mimeType = this.fileData.type;
-  if (mimeType.match(/image\/*/) == null) {
-    return;
+    if (this.fileData.size / 1024 / 1024 > 1) {
+      this.fileUploadResult = "File size is " + (this.fileData.size / 1024 / 1024).toFixed(2) + " MB. Max allowed file size is 1 MB. File cannot be uploaded!";
+      this.fileSizeError = true;
+      return;
+    }
+    else if (this.fileData.size == 0) {
+      this.fileUploadResult = "File is empty. File cannot be uploaded!";
+      this.fileSizeError = true;
+      return;
+    }
+    else {
+      this.fileSizeError = false;
+      this.fileUploadResult = "";
+    }
   }
 
-  var reader = new FileReader();      
-  reader.readAsDataURL(this.fileData); 
-  reader.onload = (_event) => { 
-    this.previewUrl = reader.result; 
-  }
-}
-
-onSubmit() {
-  const formData = new FormData();
+  onSubmit() {
+    const formData = new FormData();
     formData.append('file', this.fileData);
-    
-    //http://localhost:5000/competitionapi/v1/competition/raw
-    
-    
-    //this.http.post('http://localhost:5000/competitionapi/v1/competition/raw', formData)
-    /*this.http.post('http://localhost:5000/competitionapi/v1/competition/raw', {
-      "fileName": "a",
-      "rawFileContent": "b"
-    })
-      .subscribe(res => {
-        console.log(res);
-        //this.uploadedFilePath = res.data.filePath;
-        alert('SUCCESS !!');
-      })
-    */
-    
-      console.log(this.fileData.name);
-    
 
-     var reader = new FileReader();
+    console.log(this.fileData);
+
+    var reader = new FileReader();
     reader.onload = () => {
       this.http.post('http://localhost:5000/competitionapi/v1/competition/raw', {
-      "fileName": "a",
-      "rawFileContent": reader.result
-    })
-      .subscribe(res => {
-        console.log(res);
-        //this.uploadedFilePath = res.data.filePath;
-        //alert('SUCCESS !!');
-        this.fileUploadResult = "File is sent successfully!";
-      })
-      
-      console.log(reader.result);
-    };
+          "fileName": this.fileData.name,
+          "rawFileContent": reader.result
+        })
+          .subscribe(res => {
+            console.log(res);
+            this.fileUploadResult = "File is uploaded successfully!";
+          })
+
+        console.log(reader.result);
+      };
+
     reader.readAsText(this.fileData);
-}
 
-
-
-
+  }
 
   ngOnInit() {
   }
